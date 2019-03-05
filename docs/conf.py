435b913f5216f23844c87bf49b2c0f377e367cdb
jamesfgv <jamesjfgv@gmail.com>
2019-03-05 16:40:12 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -219,7 +219,7 @@ latex_elements = {
 'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '12pt',
+'pointsize': '12pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
