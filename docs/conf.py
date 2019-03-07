2fd6fbf90da99f43189b0d8d5cafae893ebace86
jamesfgv <jamesjfgv@gmail.com>
2019-03-07 14:41:11 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -198,7 +198,7 @@ htmlhelp_basename = 'OpenAIREGuidelinesdoc'
 custom_latex_preamble = r"""
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
-\usepackage{SanukLF-Light}
+\usepackage{SanukLF}
 \renewcommand{\familydefault}{\sfdefault}
 \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 """
