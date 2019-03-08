30b4b6fa423c4f076d48d01320d0340451e70fe6
jamesfgv <jamesjfgv@gmail.com>
2019-03-08 10:40:06 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'OpenAIREGuidelinesdoc'
 
-# Latex directives to be included directly in the latex/pdf docs.
+# Latex directives to be included directly in the latex/pdf docs. Se agregan algunas propiedades extras para el pdf
 custom_latex_preamble = r"""
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
@@ -202,7 +202,6 @@ custom_latex_preamble = r"""
 \renewcommand{\familydefault}{\sfdefault}
 \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 """
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
