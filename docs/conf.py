205441a1142d01642341121723123950507db6ba
jamesfgv <jamesjfgv@gmail.com>
2019-03-19 16:00:47 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -229,7 +229,7 @@ latex_elements = {
      			% \includegraphics[height=0.3in]{../../_static/by.png}
           %}
           %}
-          %\\renewcommand{\headrulewidth}{0.4pt}
+          %\\renewcommand{\color{blue}\headrulewidth}{0.4pt}
           %\\renewcommand{\\footrulewidth}{0.4pt}
           %\makeatother
            ''',
