1faa6ca71353c63d9ff02abe66c41398cca18fe9
jamesfgv <jamesjfgv@gmail.com>
2019-04-10 10:00:57 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -244,8 +244,9 @@ latex_documents = [
 latex_logo = '_static/piedepagina.png'
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-latex_toplevel_sectioning = 'prueba hola'
+# latex_toplevel_sectioning = 'part'
 #latex_use_parts = False
+
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
