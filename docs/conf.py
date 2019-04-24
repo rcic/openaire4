f33b094a633e6432ee26b0ac7dbfb0e490337136
jamesfgv <jamesjfgv@gmail.com>
2019-04-24 16:25:42 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -229,8 +229,7 @@ latex_elements = {
 \renewcommand{\familydefault}{\sfdefault} 
 \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 \definecolor{orange}{rgb}{236,115,29}
-\usapackage[titlesec]
-\title{orange}
+\usapackage[orange]{titlesec}
 """,
 
 }
