ca091f3b292f63aa25e552e93a05c437832637d9
jamesfgv <jamesjfgv@gmail.com>
2019-04-24 16:45:15 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -229,6 +229,7 @@ latex_elements = {
 \renewcommand{\familydefault}{\sfdefault} 
 \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 \definecolor{orange}{rgb}{236,115,29}
+\usepackage{titlesec}
 \titleformat{\section}{\color{orange}}
 """,
 
