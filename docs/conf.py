585fb4ce8fe4e91001f469f5c430745e3dae464a
jamesfgv <jamesjfgv@gmail.com>
2019-04-26 10:03:25 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -229,9 +229,16 @@ latex_elements = {
 \renewcommand{\familydefault}{\sfdefault} 
 \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 """,
-'sphinxsetup':\
-'TitleColor={rgb}{236,115,29}
-'',
+
+'sphinxsetup': \
+        'hmargin={0.7in,0.7in}, vmargin={1in,1in}, \
+        verbatimwithframe=true, \
+        TitleColor={rgb}{236,115,29}, \
+        HeaderFamily=\\rmfamily\\bfseries, \
+        InnerLinkColor={rgb}{0,0,1}, \
+        OuterLinkColor={rgb}{0,0,1}',
+
+        'tableofcontents':' ',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
