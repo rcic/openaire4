f8ad0e35e3d1b03c90e0f1fa4660e6551682ecef
jamesfgv <jamesjfgv@gmail.com>
2019-04-29 14:16:02 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -208,6 +208,7 @@ htmlhelp_basename = 'RedColDirecttrices'
 
 
 # -- Options for LaTeX output ---------------------------------------------
+latex_engine: 'xelatex'
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 'papersize': 'a4paper',
