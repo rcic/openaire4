72040333489b0eb6d26b9788dd18cefaf08a031d
jamesfgv <jamesjfgv@gmail.com>
2019-04-29 15:07:48 -0500

    james

--- docs/conf.py
+++ docs/conf.py
@@ -227,7 +227,7 @@ latex_elements = {
       \renewcommand{\familydefault}{\sfdefault}
       \RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
 ''',
-'fncychap': '\\usapackage{color}\\definecolor[rgb]{236,115,29}',
+'fncychap': '\\usapackage{color}\\definecolor{naran}{rgb}{236,115,29}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
